f7ca203f719ab7ae330745109d364a96c36815a5
bajadevmx <91626603+bajadevmx@users.noreply.github.com>
2024-06-07 00:26:15 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from 'express';
    const app = express();
    const port = 5000;
-  //  app.get('/public/index.html', (req, res) => {
-  //    res.send('Hello World!');
-  //  });
+  app.use(express.static('public'))
+   app.get('/', (req, res) => {
+     res.sendFile('index.html', {root:'public'});
+   });
    app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
